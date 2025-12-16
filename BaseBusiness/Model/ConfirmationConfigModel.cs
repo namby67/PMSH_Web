87a7c8d8c86d2da546301eda80f35c11e816d145
phuclx204 <lxp18001@gmail.com>
2025-12-16 16:07:48 +0700

    done confirmation config

--- BaseBusiness/Model/ConfirmationConfigModel.cs
+++ BaseBusiness/Model/ConfirmationConfigModel.cs
@@ -5,7 +5,7 @@ namespace BaseBusiness.Model
     public class ConfirmationConfigModel : BaseModel
     {
         public int ID { get; set; }
-        public string Email { get; set; }
+        public string EmailAddress { get; set; }
         public string MailUser { get; set; }
         public string MailPassword { get; set; }
         public string ServerName { get; set; }
@@ -14,7 +14,7 @@ namespace BaseBusiness.Model
         public string MailBody { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
-        public string UpdateBy { get; set; }
+        public string UpdatedBy { get; set; }
         public DateTime UpdatedDate { get; set; }
         public string MailSubjectENG { get; set; }
         public string MailBodyENG { get; set; }
