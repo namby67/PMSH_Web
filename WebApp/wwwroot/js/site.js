ac993ab9e8ded451ce06a1e4bcf28e1518029cc6
TuanHannciD <doba2311@gmail.com>
2025-12-19 10:27:51 +0700

    Changes to the Main Components

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Hàm gọi để lấy business-date
+
+function getBusinessDate(callback) {
+  return $.ajax({
+    url: "/Reservation/GetBusinessDate",
+    type: "get",
+    dataType: "json",
+    success: function (result) {
+      const businessDate = result.split("T")[0];
+      $("[data-business-date]").each(function () {
+
+        $(this).val(businessDate);
+      });
+
+      if (typeof callback === "function") {
+        callback(businessDate);
+      }
+    },
+    error: function (xhr) {
+      console.error(" AJAX ERROR", xhr.status);
+    },
+  });
+}
+$(document).ready(function () {
+  getBusinessDate();
+});
